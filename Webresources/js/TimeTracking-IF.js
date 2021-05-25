72c68e28115338810f7e47e8650dc597dcfc4534
IrynaJuriivna <45572834+IrynaJuriivna@users.noreply.github.com>
2021-05-25 15:38:34 +0300

    add TimeTrackingRecordForms

--- Webresources/js/TimeTracking-IF.js
+++ Webresources/js/TimeTracking-IF.js
@@ -1,5 +1,6 @@
 ﻿var Enavate = Enavate || {};
 if (!Enavate.TimeTracking) Enavate.TimeTracking = {};
+if (!Enavate.TimeTracking.Forms) Enavate.TimeTracking.Forms = {};
 
 Enavate.TimeTracking.Utils = {
     //+
@@ -48,3 +49,123 @@ Enavate.TimeTracking.Utils = {
     }
 }
 
+Enavate.TimeTracking.Forms.TimeTrackingRecordForms = {
+    formOnLoaяd: function (executionContext) {
+        var formContext = executionContext.getFormContext();
+        let currentRecordId = formContext.data.entity.getId();
+        if (!currentRecordId) {
+            Enavate.TimeTracking.Utils.setLookupFieldToCurrentUser(formContext, "st08_employee");
+        }
+        formContext.getAttribute("st08_reportedhours").addOnChange(this.onReportHoursChangeHandler.bind(this));
+        formContext.getAttribute("st08_employee").addOnChange(this.onEmployeeChangeHandler.bind(this));
+        formContext.getAttribute("st08_date").addOnChange(this.onDateChangeHandler.bind(this));
+        //      formContext.data.entity.addOnSave(this.onReportHoursSaveHandler.bind(this));
+        formContext.data.entity.addOnSave(this.formOnSaveHandler.bind(this));
+    },
+
+    onReportHoursSaveHandler: function (executionContext) {
+        //   if(executionContext.getEventArgs().getIsSaveSuccess())
+        let formContext = executionContext.getFormContext();
+        let dayIdRef = formContext.getAttribute("st08_day").getValue();
+        let dayId = (dayIdRef) ? dayIdRef[0].id : null;
+        Enavate.TimeTracking.Utils.calcRollupField(formContext, "st08_timetrackingdaies", dayId, "st08_totalhours");
+        //    }
+    },
+
+    onReportHoursChangeHandler: function (executionContext) {
+        var formContext = executionContext.getFormContext();
+        let reportedhours = formContext.getAttribute("st08_reportedhours").getValue();
+
+        if (reportedhours > 8) {
+            formContext.ui.setFormNotification("Please look to 'Reported hours' field. It shouldn`t be more then 8 hours!", "WARNING", "HoursNotification")
+        } else {
+            formContext.ui.clearFormNotification("HoursNotification")
+        }
+    },
+
+    onEmployeeChangeHandler: function (executionContext) {
+        this.setDayRecord(executionContext);
+    },
+
+    //-
+    onDateChangeHandler: function (executionContext) {
+        this.setDayRecord(executionContext);
+
+        var formContext = executionContext.getFormContext();
+        let selectedDate = formContext.getAttribute("st08_date").getValue();
+
+        //При зміні дати додаєм фільтр
+        if (selectedDate) {
+            formContext.getControl("st08_project").addPreSearch(this.setProjectLookupFilter);
+            //     formContext.ui.setFormNotification("1", "WARNING", "HoursNotification");
+        } else {
+            formContext.getControl("st08_project").removePreSearch(this.setProjectLookupFilter);
+        }
+    },
+
+    setDayRecord: function (executionContext) {
+        var formContext = executionContext.getFormContext();
+        var ttdate = formContext.getAttribute("st08_date").getValue();
+        let employeeRef = formContext.getAttribute("st08_employee").getValue();
+        let epmloyeeId = (employeeRef && employeeRef.length > 0) ? employeeRef[0].id : null;
+
+        if (ttdate && epmloyeeId) {
+            let dateStr = Enavate.TimeTracking.Utils.formatDate(ttdate);
+            console.log(dateStr);
+
+            Xrm.WebApi.retrieveMultipleRecords("st08_timetrackingday", `?$select=st08_name,st08_timetrackingdayid&$filter=st08_date eq ${dateStr} and _st08_employee_value eq ${epmloyeeId}&$top=1`).then(
+                function success(rezult) {
+                    if (rezult.entities.length > 0) {
+                        let id = rezult.entities[0].st08_timetrackingdayid;
+                        let name = rezult.entities[0].st08_name;
+                        Enavate.TimeTracking.Utils.setLookupField(formContext, "st08_day", "st08_timetrackingday", id, name);
+                    } else {
+                        formContext.getAttribute("st08_day").setValue(null);
+                    }
+                    formContext.getAttribute("st08_day").fireOnChange();
+                },
+                function (error) {
+                    console.log(error.message);
+                }
+            )
+        } else {
+            formContext.getAttribute("st08_day").setValue(null);
+        }
+    },
+
+    //- (calcRollup)
+    formOnSaveHandler: function (executionContext) {
+        var eventArgs = executionContext.getEventArgs();
+        if (eventArgs.getSaveMode() == 70) {
+            eventArgs.preventDefault();
+            return;
+        }
+        var formContext = executionContext.getFormContext();
+        let id = formContext.data.entity.getId();
+        var dayIdRef = formContext.getAttribute("st08_day").getValue();
+        var thisObject = this;
+
+        if (id && (dayIdRef && dayIdRef.length > 0)) {
+            let dayId = dayIdRef[0].id;
+
+            setTimeout(() => {
+                thisObject.allowSaveUpdate = false;
+                Enavate.TimeTracking.Utils.calcRollupField(formContext, "st08_timetrackingdaies", dayId, "st08_totalhours");
+                //Xrm.Utility.openEntityForm(entity, id);
+                formContext.data.refresh(false);
+            }, 500);
+        }
+    },
+
+    //-
+    setProjectLookupFilter: function (executionContext) {
+        var formContext = executionContext.getFormContext();
+        var selectedDate = formContext.getAttribute("st08_date").getValue();
+
+        if (selectedDate) {
+            let dateStr = Enavate.TimeTracking.Utils.formatDate(selectedDate);
+            let filter = `<filter><filter type="or" ><condition attribute="st08_startdate" operator="le" value="${dateStr}" /><condition attribute="st08_startdate" operator="null" /></filter><filter type="or" ><condition attribute="st08_enddate" operator="null" /><condition attribute="st08_enddate" operator="ge" value="${dateStr}" /></filter></filter>`
+            formContext.getControl("st08_project").addCustomFilter(filter);
+        }
+    }
+}
